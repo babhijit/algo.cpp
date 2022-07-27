091c1c028defcfdcc1c3d799e97b0665428f3c9c
babhijit <abhijitb@gmail.com>
2022-07-27 19:45:58 +0530

    Converted algo.cpp into an INTERFACE ONLY library
    TODO: Complete the test coverage for Graph

--- tests/cpp/ds/graph/graph_tests.cpp
+++ tests/cpp/ds/graph/graph_tests.cpp
@@ -23,3 +23,11 @@ TEST_F(GraphTests, InitFail) {
     using namespace algo::cpp::ds;
     ASSERT_THROW(Graph<int>(-1), std::invalid_argument);
 }
+
+TEST_F(GraphTests, Graph_5V) {
+    using namespace algo::cpp::ds;
+    constexpr int VERTICES = 5;
+    Graph<int> G(VERTICES);
+
+    ASSERT_EQ(VERTICES, G.V());
+}
