8e0abb9458a745b701b32b0433cc22e6cd0d5b6d
babhijit <abhijitb@gmail.com>
2023-02-06 04:14:00 +0530

    Implemented: *  Kth Largest Element in an Array  : LeetCode 215

--- src/cpp/problems/leetcode/pq/k_largest_215/k_largest.cpp
+++ src/cpp/problems/leetcode/pq/k_largest_215/k_largest.cpp
@@ -6,7 +6,7 @@
 namespace algo::problems::leetcode::pq::k_largest {
 
     int KLargest::findKthLargest(std::vector<int> &nums, int k) {
-        std::priority_queue<int, std::vector<int>, std::less<int>> pq(nums.begin(), nums.end());
+        std::priority_queue<int, std::vector<int>, std::less<>> pq(nums.begin(), nums.end());
 
         while(--k > 0) {
             pq.pop();
