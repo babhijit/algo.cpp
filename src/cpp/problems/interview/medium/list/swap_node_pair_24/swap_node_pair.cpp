c6293918ccd052a2cf9879bebe8879568f4bbb10
Abhijit Bandyopadhyay <abhijitb@gmail.com>
2024-07-13 22:42:01 +0530

    leetcode 24: Swap Node Pairs Time Complexity = O(N) Space Complexity = O(N) Tag: Stack, LinkedList

--- src/cpp/problems/interview/medium/list/swap_node_pair_24/swap_node_pair.cpp
+++ src/cpp/problems/interview/medium/list/swap_node_pair_24/swap_node_pair.cpp
@@ -3,49 +3,41 @@
 #include <stack>
 #include <utility>
 
+/**
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+
 namespace algo::interview::medium::ll::swap_node_pairs {
 
-    ListNode* swapNodePairs(ListNode* first, ListNode* second) {
-        if (not second) {
-            return first;
-        }
+    using NodePair = std::pair<ListNode *, ListNode *>;
 
-        ListNode* t = second->next;
-        second->next = first;
-        first->next = t;
-        return second;
+    ListNode *swapNodePairs(ListNode *first, ListNode *second, ListNode *tail) {
+        if (second) {
+            second->next = first;
+        }
+        first->next = tail;
+
+        return second ? second : first;
     }
 
     ListNode *SwapNodePairs::swapPairs(ListNode *head) {
-        using NodePair = std::pair<ListNode*, ListNode*>;
-
         std::stack<NodePair> nodePairs;
         auto it = head;
         while (it) {
-            nodePairs.emplace(it , it->next);
+            nodePairs.emplace(it, it->next);
             it = it->next;
             if (it) {
                 it = it->next;
             }
         }
 
-        ListNode* newHead = {};
-        ListNode* t = {};
-        while(not nodePairs.empty()) {
+        ListNode *newHead = {};
+        while (not nodePairs.empty()) {
             auto nodePair = nodePairs.top();
             nodePairs.pop();
 
-            auto f = nodePair.first;
-            auto s = nodePair.second;
-            if (not s) {
-                newHead = f;
-                t = f;
-                continue;
-            }
-            s->next = f;
-            f->next = t;
-            t = s;
-            newHead = s;
+            newHead = swapNodePairs(nodePair.first, nodePair.second, newHead);
         }
 
         return newHead;
