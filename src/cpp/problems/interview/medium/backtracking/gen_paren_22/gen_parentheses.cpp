9735ac8e3d2e7d1f5a6abd81c3fcce6447bf391d
Abhijit Bandyopadhyay <abhijitb@gmail.com>
2024-07-13 18:43:53 +0530

    leetcode 22: Generate Parentheses Time Complexity = O( 2^N ) Space Complexity = O(2^N) Tag: Backtracking

--- src/cpp/problems/interview/medium/backtracking/gen_paren_22/gen_parentheses.cpp
+++ src/cpp/problems/interview/medium/backtracking/gen_paren_22/gen_parentheses.cpp
@@ -1,7 +1,5 @@
 #include "gen_parentheses.hpp"
 
-#include <set>
-
 /**
  * Time Complexity: O(2^N)
  * Space Complexity: O(2^N)
@@ -13,15 +11,14 @@ namespace algo::problems::leetcode::interview::backtracing::gen_paren {
                                  int rightParenCount,
                                  int targetBraces,
                                  std::string parenStr,
-                                 std::set<std::string> &parentheses) {
+                                 std::vector<std::string> &parentheses) {
         // terminating condition
         if (leftParenCount == targetBraces) {
             // close all open braces
-            parentheses.insert(parenStr + std::string(targetBraces - rightParenCount, ')'));
+            parentheses.push_back(parenStr + std::string(targetBraces - rightParenCount, ')'));
             return;
         }
 
-        generateParenthesesImpl(leftParenCount + 1, rightParenCount, targetBraces, "(" + parenStr, parentheses);
         generateParenthesesImpl(leftParenCount + 1, rightParenCount, targetBraces, parenStr + "(", parentheses);
 
         if (rightParenCount < leftParenCount) {
@@ -30,7 +27,7 @@ namespace algo::problems::leetcode::interview::backtracing::gen_paren {
     }
 
     std::vector<std::string> GenerateParenthesis::generateParentheses(int n) {
-        std::set < std::string > parentheses;
+        std::vector < std::string > parentheses;
 
         generateParenthesesImpl(0, 0, n, "", parentheses);
 
