b3cb9278c2c12b76477f5b89660c7ccb6fbfc213
Abhijit Bandyopadhyay <abhijitb@gmail.com>
2024-07-17 16:24:46 +0530

    leetcode 57: Insert Interval Time Complexity = O(N) Space Complexity = O(1) Tag: Arrays

--- src/cpp/problems/interview/grind75/insert_interval_57/insert_interval.cpp
+++ src/cpp/problems/interview/grind75/insert_interval_57/insert_interval.cpp
@@ -9,64 +9,31 @@ namespace algo::interview::grind75::insert_interval {
         HI = 1
     };
 
-    bool isOverlapping(std::vector<int> const &a, std::vector<int> const &b) {
-        if (a[LO] < b[LO]) {
-            return a[HI] >= b[LO];
-        }
-        return b[HI] >= a[LO];
-    }
-
     IntMatrix InsertInterval::insert(IntMatrix &intervals, std::vector<int> &newInterval) {
-        if (intervals.empty()) {
-            return {newInterval};
+        IntMatrix mergedIntervals;
+
+        int i = 0;
+        // intervals preceding newInterval
+        while((i < intervals.size()) and (newInterval[LO] > intervals[i][HI]) ) {
+            mergedIntervals.push_back(intervals[i]);
+            ++i;
         }
 
-        auto insertInterval = [&newInterval](IntMatrix &result, std::vector<int> &interval) {
-            if (result.empty()) {
-                // is newInterval before interval
-                if (newInterval[HI] < interval[LO]) {
-                    result.push_back(newInterval);
-                }
-                result.push_back(interval);
-            } else {
-                auto &runningInterval = result.back();
+        mergedIntervals.push_back(newInterval);
 
-                // check if newInterval is sandwiched between runningInterval and interval
-                if ((newInterval[LO] > runningInterval[HI]) and
-                    (newInterval[HI] < interval[LO])) {
-                    result.push_back(newInterval);
-                }
-
-                result.push_back(interval);
-            }
-
-            auto &insertedInterval = result.back();
-            if (isOverlapping(insertedInterval, newInterval)) {
-                insertedInterval[LO] = std::min(insertedInterval[LO], newInterval[LO]);
-                insertedInterval[HI] = std::max(insertedInterval[HI], newInterval[HI]);
-            }
-        };
-
-        IntMatrix nonOverlappingIntervals;
-        insertInterval(nonOverlappingIntervals, intervals[0]);
-
-        for (auto i = 1; i < intervals.size(); ++i) {
-            auto &runningInterval = nonOverlappingIntervals.back();
-            auto &interval = intervals[i];
-
-            // check if the new interval overlaps with the running interval
-            if (isOverlapping(runningInterval, interval)) {
-                runningInterval[HI] = std::max(interval[HI], runningInterval[HI]);
-            } else {
-                insertInterval(nonOverlappingIntervals, interval);
-            }
+        auto& lastInterval = mergedIntervals.back();
+        // overlapping intervals
+        while ((i < intervals.size()) and (intervals[i][LO] <= newInterval[HI])) {
+            lastInterval[LO] = std::min(lastInterval[LO], intervals[i][LO]);
+            lastInterval[HI] = std::max(lastInterval[HI], intervals[i][HI]);
+            ++i;
         }
 
-        // edge case: check if newInterval insertion at the end
-        if (newInterval[LO] > nonOverlappingIntervals.back()[HI]) {
-            nonOverlappingIntervals.push_back(newInterval);
+        while (i < intervals.size()) {
+            mergedIntervals.push_back(intervals[i]);
+            ++i;
         }
 
-        return nonOverlappingIntervals;
+        return mergedIntervals;
     }
 }
