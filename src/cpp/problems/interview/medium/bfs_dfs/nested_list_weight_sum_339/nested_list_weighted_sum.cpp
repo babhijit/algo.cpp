b82f64390c96bfa87edb7999075af60c4a899a2d
Abhijit Bandyopadhyay <abhijitb@gmail.com>
2024-07-21 09:53:12 +0530

    leetcode 339: Nested List Weighted Sum Time Complexity = O(n) Space Complexity = O(n) Tag: BFS

--- src/cpp/problems/interview/medium/bfs_dfs/nested_list_weight_sum_339/nested_list_weighted_sum.cpp
+++ src/cpp/problems/interview/medium/bfs_dfs/nested_list_weight_sum_339/nested_list_weighted_sum.cpp
@@ -1,31 +1,76 @@
 #include "nested_list_weighted_sum.hpp"
 
+#include <queue>
+#include <vector>
+
+
 namespace algo::interview::medium::array::nested_list_weighted_sum {
+
+#ifdef DFS
+
+    /**
+     * Time Complexity: O(N)
+     * Space Complexity: O(N)
+     **/
+    int weightedNumDfs(int depth, std::vector<NestedInteger> const& nestedList) {
+        int sum{0};
+
+        for(auto& ni: nestedList) {
+            if (ni.isInteger()) {
+                sum += (depth * ni.getInteger());
+            } else {
+                sum += weightedNumDfs(depth + 1, ni.getList());
+            }
+        }
+
+        return sum;
+    }
+
+#else
+    int getCurLvlSum(std::queue<NestedInteger>& q) {
+        std::queue<NestedInteger> nxtLvlQ;
+        int sum{0};
+
+        while(not q.empty()) {
+            auto ni = q.front();
+            q.pop();
+
+            if (ni.isInteger()) {
+                sum += ni.getInteger();
+            } else {
+                nxtLvlQ.push_range(ni.getList());
+            }
+        }
+
+        q.swap(nxtLvlQ);
+        return sum;
+    }
 
     /**
      * Time Complexity: O(N)
      * Space Complexity: O(N)
      */
-    int weightedNumDfs(int depth, NestedInteger ni) {
-        if (ni.isInteger()) {
-            return depth * ni.getInteger();
-        }
+    int weightedSumBfs(std::vector<NestedInteger> const& nestedList) {
+        int sum{0};
 
-        auto &niList = ni.getList();
-        int weightedNumber{};
-        for (auto &nestedInt: niList) {
-            weightedNumber += weightedNumDfs(depth + 1, nestedInt);
-        }
-        return weightedNumber;
-    }
+        std::queue<NestedInteger> q;
+        q.push_range(nestedList);
 
-    int NestedListWeightedSum::depthSum(std::vector<NestedInteger> &nestedList) {
-        int sum{};
-
-        for (auto &ni: nestedList) {
-            sum += weightedNumDfs(1, ni);
+        int depth = 1;
+        while(not q.empty()) {
+            sum += depth * getCurLvlSum(q);
+            ++depth;
         }
 
         return sum;
+    }
+#endif
+
+    int NestedListWeightedSum::depthSum(std::vector<NestedInteger> &nestedList) {
+#ifdef DFS
+        return weightedNumDfs(1, nestedList);
+#else
+        return weightedSumBfs(nestedList);
+#endif
     }
 }
