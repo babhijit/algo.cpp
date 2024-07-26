b17ac70e6873e932edbfaf6cb1e4ebb336420872
Abhijit Bandyopadhyay <abhijitb@gmail.com>
2024-07-26 21:44:49 +0530

    leetcode 560: Subarray Sum Equals K Time Complexity = O( n ) Space Complexity = O( n ) Tag: Presum;HashTable

--- src/cpp/problems/interview/medium/array/sub_arr_sum_eq_k_560/sub_array_sums.cpp
+++ src/cpp/problems/interview/medium/array/sub_arr_sum_eq_k_560/sub_array_sums.cpp
@@ -1,29 +1,32 @@
 #include "sub_array_sums.hpp"
 
+#include <unordered_map>
+
 namespace algo::interview::medium::array::sub_array_sum {
 
+    /**
+     * Time Complexity : O(n)
+     * Space Complexity: O(n)
+     */
+
     int SubArraySum::subarraySum(std::vector<int> &nums, int k) {
+        std::unordered_map<int, int> prefixSum;
+        prefixSum[0] = 1;
 
-        std::vector<int> preSum(nums.size(), 0);
+        int sum{0};
+        int numOfSubarrays{0};
+        for (auto &num: nums) {
+            sum += num;
+            auto remainder = sum - k;
+            if (prefixSum.contains(remainder)) {
+                numOfSubarrays += prefixSum[remainder];
+            }
 
-        preSum[0] = nums[0];
-        for(auto i = 1; i < nums.size(); ++i) {
-            preSum[i] = preSum[i-1] + nums[i];
+            auto &count = prefixSum[sum];
+            ++count;
         }
 
-        int numOfSubarray{0};
-        for (int i = 0; i < preSum.size(); ++i) {
-            if (preSum[i] == k) {
-                ++numOfSubarray;
-            }
-            for (int j = i + 1; j < preSum.size(); ++j) {
-                if (preSum[j] - preSum[i] == k) {
-                    ++numOfSubarray;
-                }
-            }
-        }
-
-        return numOfSubarray;
+        return numOfSubarrays;
     }
 
 }
