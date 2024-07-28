f7e9796626cb6587c28ad3cb86d5130407b8aa3c
Abhijit Bandyopadhyay <abhijitb@gmail.com>
2024-07-28 06:38:50 +0530

    leetcode 973: Closest K points from Origin Time Complexity = O( N * log(K ) Space Complexity = O( K ) Tag: PriorityQueue

--- src/cpp/problems/interview/medium/array/k_closest_pts_to_origin_973/k_closest_points_to_origin.cpp
+++ src/cpp/problems/interview/medium/array/k_closest_pts_to_origin_973/k_closest_points_to_origin.cpp
@@ -3,8 +3,8 @@
 #include <queue>
 
 /**
- * Time Complexity: N * log(N)
- * Space Complexity: N
+ * Time Complexity: N * log(k)
+ * Space Complexity: k
  */
 
 namespace algo::interview::medium::array::k_closest_pt_to_origin {
@@ -23,21 +23,26 @@ namespace algo::interview::medium::array::k_closest_pt_to_origin {
 
     struct PointComparator {
         bool operator()(Point const &a, Point const &b) {
-            return d2O(a) >d2O(b);
+            return d2O(a) < d2O(b);
         }
     };
 
     Matrix KClosestPointsToOrigin::kClosest(Matrix &points, int k) {
+        // create a max pq
         std::priority_queue < Point, Matrix, PointComparator > pq;
         for (auto &pt: points) {
             pq.push(pt);
+            if (pq.size() > k) {
+                // pop the 'greater than' element
+                pq.pop();
+            }
         }
 
-        Matrix closestPoints;
-        while (k > 0) {
-            closestPoints.push_back(pq.top());
+        // push in reverse order
+        Matrix closestPoints(k, {0, 0});
+        while (not pq.empty()) {
+            closestPoints[--k] = pq.top();
             pq.pop();
-            --k;
         }
 
         return closestPoints;
