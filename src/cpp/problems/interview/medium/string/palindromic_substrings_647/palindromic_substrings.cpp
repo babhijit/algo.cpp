d22c19fad8840733f488aa55240c367d01d927d2
Abhijit Bandyopadhyay <abhijitb@gmail.com>
2024-08-10 10:51:46 +0530

    leetcode 647: Palindromic Substrings Time Complexity = O( N^2) Space Complexity = O( N^2 ) Tag: String;TwoPointers;DynamicProgramming;Memoisation

--- src/cpp/problems/interview/medium/string/palindromic_substrings_647/palindromic_substrings.cpp
+++ src/cpp/problems/interview/medium/string/palindromic_substrings_647/palindromic_substrings.cpp
@@ -3,33 +3,42 @@
 #include <utils/dstypedefs.hpp>
 
 /**
- * Space Complexity: O(1)
- * Time Complexity: O(N^3)
+ * Space Complexity: O(N^2)
+ * Time Complexity: O(N^2)
  */
+
+using namespace algo::cpp::ds::utils;
 
 namespace algo::interview::medium::string::palindromic_substring {
 
-    bool isPalindrome(std::string const& s, int low, int high) {
+    bool isPalindrome(std::string const& s, int low, int high, Matrix<int>& dp) {
         int left{low}, right{high};
 
         while (left < right) {
+            if (dp[left][right] != -1) {
+                return dp[left][right] != 0;
+            }
+
             if (s[left] != s[right]) {
+                dp[left][right] = 0;
                 return false;
             }
             ++left;
             --right;
         }
+
+        dp[low][high] = 1;
         return true;
     }
 
     int PalindromicSubstrings::countSubstrings(const std::string &s) {
         using namespace algo::cpp::ds::utils;
-        Matrix<int> dp;
+        Matrix<int> dp { s.size(), std::vector<int>(s.size(), -1) };
         int count{0};
 
         for (int i = 0; i < s.size(); ++i) {
             for (int j = i; j < s.size(); ++j) {
-                if (isPalindrome(s, i, j)) {
+                if (isPalindrome(s, i, j, dp)) {
                     ++count;
                 }
             }
