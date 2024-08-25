c39076087c64425db799c075912e28bd05d87cfe
Abhijit Bandyopadhyay <abhijitb@gmail.com>
2024-08-25 07:56:13 +0530

    leetcode 958: Check Completeness of Binary Tree Time Complexity = O( N ) Space Complexity = O( N ) Tag: Tree,BFS

--- src/cpp/problems/interview/medium/tree/check_tree_completeness_958/complete_btree.cpp
+++ src/cpp/problems/interview/medium/tree/check_tree_completeness_958/complete_btree.cpp
@@ -10,73 +10,26 @@
 
 namespace algo::interview::medium::tree::complete_btree {
 
-    inline bool isLeafNode(TreeNode* node) {
-        return not node->left and not node->right;
-    }
-
-    inline bool isNodeIncomplete(TreeNode* node) {
-        return not node->left and node->right;
-    }
-
-    bool isLevelComplete(std::queue<TreeNode *> &nodesAtLevel) {
-        std::queue<TreeNode *> nextLevel;
-
-        bool prevNodeHasBothChildren{true};
-        while (not nodesAtLevel.empty()) {
-            auto node = nodesAtLevel.front();
-            nodesAtLevel.pop();
-
-            // if the previous node has no children
-            // then the current node should not have any children for tree completeness
-            if (not prevNodeHasBothChildren) {
-                if (node->left or node->right) {
-                    return false;
-                }
-            }
-
-            if (isLeafNode(node)) {
-                prevNodeHasBothChildren = false;
-                continue;
-            }
-
-            if (isNodeIncomplete(node)) {
-                return false;
-            }
-
-            prevNodeHasBothChildren = node->left and node->right;
-            nextLevel.push(node->left);
-            if (node->right) {
-                nextLevel.push(node->right);
-            }
-        }
-
-        std::swap(nextLevel, nodesAtLevel);
-        return true;
-    }
-
-
     bool CompleteBinaryTree::isCompleteTree(TreeNode *root) {
         std::queue<TreeNode *> nodes;
 
         nodes.push(root);
-        int level = 0;
-        bool canProcessNextLevel{true};
+        bool noPrevSibling {false};
         while (not nodes.empty()) {
-            if (not canProcessNextLevel) {
+            auto node = nodes.front();
+            nodes.pop();
+            if (not node) {
+                noPrevSibling = true;
+                continue;
+            }
+
+            // once there are no previous siblings, nodes following is not expected
+            if(noPrevSibling) {
                 return false;
             }
 
-            // we should go to next level only if the previous level had all children
-            auto expectedLevelCompleteNodeCount = 1 << level;
-            if (expectedLevelCompleteNodeCount != nodes.size()) {
-                canProcessNextLevel = false;
-            }
-
-            if (not isLevelComplete(nodes)) {
-                return false;
-            }
-
-            ++level;
+            nodes.push(node->left);
+            nodes.push(node->right);
         }
 
         return true;
